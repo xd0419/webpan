ab2ba07236423798acaf3345eaa5c62205d0e243
xd0419 <1249457656@qq.com>
2020-06-29 18:07:58 +0800

    添加apply实体类

---
+++ webpan/src/main/java/webpan/model/Apply.java
@@ -0,0 +1,41 @@
+package webpan.model;
+
+public class Apply {
+	private int ApplyID;
+	private int ApplySize;
+	private boolean ApplyStatus;
+	private String ApplyUser;
+	private String ApplyManager;
+	
+	public int getApplyID() {
+		return ApplyID;
+	}
+	public void setApplyID(int applyID) {
+		ApplyID = applyID;
+	}
+	public int getApplySize() {
+		return ApplySize;
+	}
+	public void setApplySize(int applySize) {
+		ApplySize = applySize;
+	}
+	public boolean getApplyStatus() {
+		return ApplyStatus;
+	}
+	public void setApplyStatus(boolean applyStatus) {
+		ApplyStatus = applyStatus;
+	}
+	public String getApplyUser() {
+		return ApplyUser;
+	}
+	public void setApplyUser(String applyUser) {
+		ApplyUser = applyUser;
+	}
+	public String getApplyManager() {
+		return ApplyManager;
+	}
+	public void setApplyManager(String applyManager) {
+		ApplyManager = applyManager;
+	}
+	
+}
